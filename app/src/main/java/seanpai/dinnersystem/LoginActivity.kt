ee23cbeda053377094f9b060428a1e51f48617a6
Sean <seanpai96@gmail.com>
2018-12-08 01:53:48 +0800

    login layout

--- app/src/main/java/seanpai/dinnersystem/LoginActivity.kt
+++ app/src/main/java/seanpai/dinnersystem/LoginActivity.kt
@@ -3,6 +3,7 @@ package seanpai.dinnersystem
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
