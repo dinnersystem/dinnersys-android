9f2b778fe2be7bd969a0e016120c63c7088b7c09
Sean <seanpai96@gmail.com>
2019-02-01 13:25:58 +0800

    added firebase, blank HistoryActivity

---
+++ app/src/main/java/seanpai/dinnersystem/MainHistoryActivity.kt
@@ -0,0 +1,12 @@
+package seanpai.dinnersystem
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_history)
+    }
+}
