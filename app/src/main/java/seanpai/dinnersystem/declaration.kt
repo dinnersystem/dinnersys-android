c06511d9ef77b14d9125c810f7aaccc73ae6469b
Sean <seanpai96@gmail.com>
2019-10-20 15:45:21 +0800

    auto rotation fix

--- app/src/main/java/seanpai/dinnersystem/declaration.kt
+++ app/src/main/java/seanpai/dinnersystem/declaration.kt
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 //variable
-var currentVersion = 201900022
+var currentVersion = 201900023
 var userInfo = JSONObject("{}")
 var posInfo = JSONObject("{}")
 var allMenuJson = JSONArray("[]")
